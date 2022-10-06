143db73d8068c77a3930932f2643209a47b0a90f
laksmig6 <lakshmig@thoughtworks.com>
2022-10-06 09:19:09 +0530

    useSearchParam example

--- src/pages/UsersContainer.js
+++ src/pages/UsersContainer.js
@@ -1,16 +1,28 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Outlet, useSearchParams } from "react-router-dom";
 
 const UsersContainer = () => {
+  const [searchParam, setSearchParam] = useSearchParams({ n: 3 });
+  const number = searchParam.get("n");
+
   return (
     <>
       <Link to="/users/1">User 1</Link>
       <br />
       <Link to="/users/2">User 2</Link>
       <br />
+      <Link to={`/users/${number}`}>User {number}</Link>
+      <br />
       <Link to="/users/new">New User</Link>
       <br />
       <Outlet context={{ Hello: "world" }} />
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => {
+          setSearchParam({ n: e.target.value });
+        }}
+      />
     </>
   );
 };
